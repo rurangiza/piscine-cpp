3a961cb47edb03948d545378044ca8e9463c84db
Arsene Rurangiza <arurangi@shi-r9-p2.s19.be>
2023-05-31 15:41:06 +0200

    dojo: adds classes initializations

--- dojo/Sample.class.cpp
+++ dojo/Sample.class.cpp
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:42:58 by arurangi          #+#    #+#             */
-/*   Updated: 2023/05/31 15:15:31 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:28:25 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,19 @@ void
 Sample::bar(void)
 {
 	std::cout << "Des barres de chocolats" << std::endl;
+	return;
+}
+
+Sample2::Sample2(char p1, int p2, float p3) : a1(p1), a2(p2), a3(p3)
+{
+	std::cout << "Constructor called" << std::endl;
+	std::cout << "this->a1 = " << this->a1 << std::endl;
+	std::cout << "this->a2 = " << this->a2 << std::endl;
+	std::cout << "this->a3 = " << this->a3 << std::endl;
+}
+
+Sample2::~Sample2(void)
+{
+	std::cout << "Destructor called" << std::endl;
 	return;
 }
--- dojo/Sample.class.hpp
+++ dojo/Sample.class.hpp
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 14:41:16 by arurangi          #+#    #+#             */
-/*   Updated: 2023/05/31 14:59:23 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/05/31 15:24:44 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,28 @@
 # define SAMPLE_CLASS_H
 
 class Sample {
-	
+
 	public:
 	
-		int	foo;
+		int		foo;
 		
 		Sample(void); // constructor
 		~Sample(void); // destructor
 		
 		void	bar(void);
+
+};
+
+class Sample2 {
+
+	public:
+		char	a1;
+		int		a2;
+		float	a3;
+		
+		Sample2(char p1, int p2, float p3);
+		~Sample2(void);
+
 };
 
 #endif
