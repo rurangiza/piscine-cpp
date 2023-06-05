1fa3244da73a4fa8293324d0aa3749ce3947b721
Arsene <a.rurangiza@gmail.com>
2023-06-05 14:10:36 +0200

    updated

---
+++ 00/ex02/Account.cpp
@@ -0,0 +1,73 @@
+
+#include "Account.hpp"
+
+//===---===---===---===---===---===---===---===---===---===---===---===---===---
+
+Account::Account(int initial_deposit) {
+    ;
+}
+
+Account::~Account() {
+    ;
+}
+
+//===---===---===---===---===---===---===---===---===---===---===---===---===---
+
+int
+Account::getNbAccounts() {
+    return 0;
+}
+
+int
+Account::getTotalAmount() {
+    return 0;
+}
+
+int
+Account::getNbDeposits() {
+    return 0;
+}
+
+int
+Account::getNbWithdrawals() {
+    return 0;
+}
+
+void
+Account::displayAccountsInfos() {
+    ;
+}
+
+//===---===---===---===---===---===---===---===---===---===---===---===---===---
+
+void
+Account::makeDeposit(int deposit) {
+    ;
+}
+
+bool
+Account::makeWithdrawal(int withdrawal) {
+    return true;
+}
+
+int
+Account::checkAmount() const {
+    return 0;
+}
+
+void
+Account::displayStatus() const {
+    ;
+}
+
+// PRIVATE ===---===---===---===---===---===---===---===---===---===---===---===
+
+void
+Account::_displayTimestamp() {
+    ;
+}
+
+Account::Account() {
+    ;
+}
+
--- 00/ex02/tests.cpp
+++ 00/ex02/tests.cpp
@@ -12,55 +12,62 @@
 #include <functional>
 #include "Account.hpp"
 
-
 int		main( void ) {
+    // Setup: renaming types
+    typedef std::vector<Account::t>                             accounts_t;
+    typedef std::vector<int>                                    ints_t;
+    typedef std::pair<accounts_t::iterator, ints_t::iterator>   acc_int_t;
 
-	typedef std::vector<Account::t>							  accounts_t;
-	typedef std::vector<int>								  ints_t;
-	typedef std::pair<accounts_t::iterator, ints_t::iterator> acc_int_t;
+    // Init: accounts (values, size, start, end)
+    int	const               amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
+    size_t const            amounts_size = sizeof(amounts) / sizeof(int);
+    accounts_t              accounts( amounts, amounts + amounts_size );
+    accounts_t::iterator    acc_begin	= accounts.begin();
+    accounts_t::iterator    acc_end = accounts.end();
 
-	int	const				amounts[]	= { 42, 54, 957, 432, 1234, 0, 754, 16576 };
-	size_t const			amounts_size( sizeof(amounts) / sizeof(int) );
-	accounts_t				accounts( amounts, amounts + amounts_size );
-	accounts_t::iterator	acc_begin	= accounts.begin();
-	accounts_t::iterator	acc_end		= accounts.end();
+    // Init: deposits (values, size, start, end)
+    int	const           d[] = { 5, 765, 564, 2, 87, 23, 9, 20 };
+    size_t const        d_size( sizeof(d) / sizeof(int) );
+    ints_t              deposits( d, d + d_size );
+    ints_t::iterator    dep_begin = deposits.begin();
+    ints_t::iterator    dep_end = deposits.end();
 
-	int	const			d[]			= { 5, 765, 564, 2, 87, 23, 9, 20 };
-	size_t const		d_size( sizeof(d) / sizeof(int) );
-	ints_t				deposits( d, d + d_size );
-	ints_t::iterator	dep_begin	= deposits.begin();
-	ints_t::iterator	dep_end		= deposits.end();
+    // Init: withdrawals (values, size, start, end)
+    int	const           w[] = { 321, 34, 657, 4, 76, 275, 657, 7654 };
+    size_t const        w_size( sizeof(w) / sizeof(int) );
+    ints_t              withdrawals( w, w + w_size );
+    ints_t::iterator    wit_begin	= withdrawals.begin();
+    ints_t::iterator    wit_end = withdrawals.end();
 
-	int	const			w[]			= { 321, 34, 657, 4, 76, 275, 657, 7654 };
-	size_t const		w_size( sizeof(w) / sizeof(int) );
-	ints_t				withdrawals( w, w + w_size );
-	ints_t::iterator	wit_begin	= withdrawals.begin();
-	ints_t::iterator	wit_end		= withdrawals.end();
+    // Display account infos
+    Account::displayAccountsInfos();
+    std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+    // Make deposits
+    for ( acc_int_t it( acc_begin, dep_begin );
+        it.first != acc_end && it.second != dep_end;
+        ++(it.first), ++(it.second) ) {
 
-	for ( acc_int_t it( acc_begin, dep_begin );
-		  it.first != acc_end && it.second != dep_end;
-		  ++(it.first), ++(it.second) ) {
+            (*(it.first)).makeDeposit( *(it.second) );
+    }
 
-		(*(it.first)).makeDeposit( *(it.second) );
-	}
+    // Display accounts info (again)
+    Account::displayAccountsInfos();
+    std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
+    // Make withdrawals
+    for ( acc_int_t it( acc_begin, wit_begin ); // for loop (1st arg)
+        it.first != acc_end && it.second != wit_end;    // for loop (2nd arg)
+        ++(it.first), ++(it.second) ) {                 // for loop (3rd arg)
 
-	for ( acc_int_t it( acc_begin, wit_begin );
-		  it.first != acc_end && it.second != wit_end;
-		  ++(it.first), ++(it.second) ) {
+            (*(it.first)).makeWithdrawal( *(it.second) );
+    }
 
-		(*(it.first)).makeWithdrawal( *(it.second) );
-	}
+    // Display account infos (last time)
+    Account::displayAccountsInfos();
+    std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
 
-	Account::displayAccountsInfos();
-	std::for_each( acc_begin, acc_end, std::mem_fun_ref( &Account::displayStatus ) );
-
-	return 0;
+    return 0;
 }
 
 
