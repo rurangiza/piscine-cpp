c26167e58ac65c5eefc0c7574296bd2b6c1dc253
Arsene <a.rurangiza@gmail.com>
2023-06-16 16:30:56 +0200

    bug fix: adds condition to ignoring space

--- 00/ex01/src/helpers.cpp
+++ 00/ex01/src/helpers.cpp
@@ -60,23 +60,19 @@ is_all_spaces(std::string str) {
 
 std::string
 ignoreSpaces(std::string str) {
-    // std::istringstream iss(str);
 
-    // std::string word;
-    // while (iss >> word)
-    //     ;
-    // return word;
+    if (str.size() > 10) {
+        size_t start = 0;
+        while (start < str.size() && isspace(str[start]))
+            start++;
 
-    size_t start = 0;
-    while (start < str.size() && isspace(str[start]))
-        start++;
-
-    size_t end = str.size() - 1;
-    while (end > 0 && isspace(str[end]))
-        end--;
-    
-    return str.substr(start, end);
-
+        size_t end = str.size() - 1;
+        while (end > 0 && isspace(str[end]))
+            end--;
+        
+        return str.substr(start, end);
+    }
+    return str;
 }
 
 void
