a18169ac2f08a9a827a9b354d2564bd2baa907f6
Arsene <a.rurangiza@gmail.com>
2023-07-07 10:59:57 +0200

    updated

---
+++ 03/ex01/includes/constants.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   constants.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 18:09:10 by arurangi          #+#    #+#             */
+/*   Updated: 2023/07/05 19:48:50 by arurangi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COMMON_H
+    #define COMMON_H
+
+    #define CBLACK      "\033[0;30m"
+    #define CGRAY       "\033[90m"
+    #define CRED        "\033[0;31m"
+    #define CGREEN      "\033[0;32m"
+    #define CBLUE       "\033[0;94m"
+    #define CYELLOW     "\033[0;33m"
+    #define CMAGENTA    "\033[0;35m"
+    #define CRESET      "\033[0m"
+    #define CBOLD       "\x1b[1m"
+
+#endif
---
+++ 03/ex01/src/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/04 18:09:00 by arurangi          #+#    #+#             */
+/*   Updated: 2023/07/07 10:02:38 by arurangi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/ClapTrap.hpp"
+#include "../includes/UserInterface.hpp"
+
+UserInterface ui2;
+
+void Test_constructors();
+void Test_validArgs();
+void Test_invalidArgs();
+
+int main()
+{
+    ui2.header();
+    ui2.iScope("main", 0);
+    
+    Test_constructors();
+    std::cout << std::endl;
+    
+    Test_validArgs();
+    std::cout << std::endl;
+    
+    Test_invalidArgs();
+
+    ui2.oScope("main", 0);
+}
+
+void Test_constructors() {
+    ClapTrap naruto("Naruto");
+    ClapTrap narutoClone(naruto);
+    narutoClone = naruto;
+}
+
+void Test_validArgs() {
+    ClapTrap sasuke("Sasuke");
+
+    sasuke.attack("Naruto");
+    sasuke.takeDamage(20);
+    sasuke.beRepaired(10);
+}
+
+void Test_invalidArgs() {
+    
+}
